f69a29ff8074782732ab9ec00f3da90632040d02
Marek Mottl <marek@czechgeeks.cz>
2013-10-14 17:49:20 +0200

    Sprite.cs class added, Game1 constructor parametres set

--- mtg_lifecounter/mtg_lifecounter/Game1.cs
+++ mtg_lifecounter/mtg_lifecounter/Game1.cs
@@ -22,6 +22,10 @@ namespace mtg_lifecounter
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
+            graphics.PreferredBackBufferWidth = 480;
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.IsFullScreen = true;
+
             // Frame rate is 30 fps by default for Windows Phone.
             TargetElapsedTime = TimeSpan.FromTicks(333333);
 
